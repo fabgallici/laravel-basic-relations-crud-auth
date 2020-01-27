4af625c7c169ace354872822a9b7d23d9594b74d
cabal82 <fabriziogallici@gmail.com>
2020-01-27 15:08:41 +0100

    view tags

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,6 +17,7 @@
                     <th>Title</th>
                     <th>Category Name</th>
                     <th>Description</th>
+                    <th>Tags</th>
                     <th>Aggiorna</th>
                     <th>Delete</th>
                 </tr>
@@ -28,6 +29,11 @@
                     <td>{{ $post->title }}</td>
                     <td>{{ $post->category->title }}</td>
                     <td>{{ \Illuminate\Support\Str::limit($post->postInformation->description, $limit = 30, $end = '...') }}</td>
+                    <td>
+                        @foreach ($post->tags as $tag)
+                            <span> {{ $tag->title }}, </span>
+                        @endforeach                 
+                    </td>
                     <td>
                         <a class="btn btn-success" href="{{ route('posts.edit', $post) }}">Aggiorna</a>
                     </td>
