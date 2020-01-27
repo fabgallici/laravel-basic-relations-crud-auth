f84a977729a80463a90652d63d977fd6669952d1
cabal82 <fabriziogallici@gmail.com>
2020-01-27 14:23:36 +0100

    migrations + db relations

--- app/Post.php
+++ app/Post.php
@@ -23,4 +23,8 @@ class Post extends Model
         return $this->belongsTo('App\Category');
     }
 
+    public function tags() {
+        return $this->belongsToMany('Tag::class');
+    }
+
 }
---
+++ database/migrations/2020_01_27_131010_add_foreign_key.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('post_tag', function (Blueprint $table) {
+            $table -> bigInteger('tag_id') -> unsigned() -> index();
+            $table -> foreign('tag_id', 'post_tag_tags') 
+                    -> references('id') 
+                    -> on('tags');
+            $table -> bigInteger('post_id') -> unsigned() -> index();
+            $table -> foreign('post_id', 'post_tag_posts') 
+                    -> references('id') 
+                    -> on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('post_tag', function (Blueprint $table) {
+            $table -> dropForeign('post_tag_tags');
+            $table -> dropForeign('post_tag_posts');
+
+            $table -> dropColumn('tag_id');
+            $table -> dropColumn('post_id');
+        });
+       
+    }
+}
