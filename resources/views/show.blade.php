04f74692690f680d9cd21431a6a91b76de2c9ff2
cabal82 <fabriziogallici@gmail.com>
2020-01-27 21:05:43 +0100

    show tags

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,9 +4,17 @@
 
   <div class="card" style="width: 18rem;">
   <div class="card-body">
-    <h5 class="card-title">{{ $post ->title }}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">{{ $post ->author }}</h6>
-    <p class="card-text">{{ $post->postInformation->description }}</p>
+    <h5 class="card-title">Title: {{ $post ->title }}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Author: {{ $post ->author }}</h6>
+    <p class="card-text">Description: {{ $post->postInformation->description }}</p>
+
+    <div> Tags:
+      @foreach ($post->tags as $tag)
+        <span>{{ $tag->title }} - </span>
+       @endforeach
+    </div>
+    
+
     <a href="{{ route('posts.index')}}" class="btn btn-primary">Back to Posts</a>
   </div>
 </div>
